1ac6fb4e6c44e1bebb07b3c7a4908be6e57cdbd8
root <root@inception.inception>
2022-12-10 19:59:15 +0100

    mariadb good should do wordpress

--- requirements/mariadb/tools/entrypoint.sh
+++ requirements/mariadb/tools/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-mysql_install_db
-mysql --ssl --protocol=tcp
+if [ ! -d /var/lib/mysql/wordpress ]; then
+  mysql_install_db
+  /usr/share/mariadb/mysql.server start
+  mysql -e "\
+    CREATE DATABASE IF NOT EXISTS $DB_NAME; \
+    CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'; \
+    GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%'; \
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'; \
+    FLUSH PRIVILEGES; \
+    "
+  mysqladmin --user=root --password=$MARIADB_ROOT_PASSWORD shutdown
+fi
+
+exec "$@"
