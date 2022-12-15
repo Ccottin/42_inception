cc9d9c56a1e8242a8b03476538efde78fbffeea7
root <root@inception.inception>
2022-12-15 12:59:05 +0100

    alright

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -10,10 +10,7 @@ done
 
 wp core is-installed
 if [ $? -eq 1 ]; then
-
-sleep 60
 wp core install --url="https://$HOSTNAME" --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_email=$WP_ADMIN_MAIL --admin_password=$WP_ADMIN_PASSWORD
-
 wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD
 fi
 
